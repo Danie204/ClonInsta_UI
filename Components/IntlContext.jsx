9e5ce7c8024237ce38f4c6c2faa247337956425a
Gonzalo Bonilla <145482713+zalo1490@users.noreply.github.com>
2024-02-06 21:14:43 +0100

    Perfil del usuario/falta conectarlo a la api

---
+++ Components/IntlContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from "react"
+import { IntlProvider } from 'react-intl'
+import en from '../locale/en.json'
+import es from '../locale/es.json'
+
+const messages = { en, es }
+
+const IntlContext = createContext()
+
+export const MyIntlProvider = ({ children }) => {
+  const [lang, setLang] = useState(navigator.language || 'en')
+
+  return (
+    <IntlContext.Provider value={[lang, setLang]}>
+      <IntlProvider messages={messages[lang] || messages.en} locale={lang} defaultLocale="en">
+        {children}
+      </IntlProvider>
+    </IntlContext.Provider>
+  )
+}
+
+export const useLang = () => useContext(IntlContext)
---
+++ Components/Loading/Loading.jsx
@@ -0,0 +1,10 @@
+import './Loading.css'
+
+const Loading = ({ className, ...others }) => {
+  const cls = className ? className + ' loading' : 'loading'
+
+  return <div className={cls} {...others} />
+}
+
+
+export default Loading
