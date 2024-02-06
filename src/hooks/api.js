4bde6824a4ec664e2b408baf1d3b9f5017562d2e
Gonzalo Bonilla <145482713+zalo1490@users.noreply.github.com>
2024-02-06 17:47:32 +0100

    Actualizo

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,18 +1,26 @@
 import { useFetch, useFetchPosts } from '/src/hooks/useFetch.js';
 
-const apiHost = 'http://localhost:3000'
+const apiHost = 'http://localhost:3000';
 
-export const usePosts = () => useFetch(apiHost + '/posts/')
+export const usePosts = () => useFetch(apiHost + '/posts/');
 
-export const useMyInfo = () => useFetch(apiHost + '/users')
+export const useMyInfo = () => useFetch(apiHost + '/users');
 
 export const useUserActions = () => {
-    const fetchPosts = useFetchPosts()
-return {
-    login: (email, password) => fetchPosts(apiHost + '/users/login', { email, password }),
-    createPosts: (description, photo) => fetchPosts(apiHost + '/posts', { description, photo }),
-    like: async (id) => fetchPosts(apiHost + '/posts/:postId/like', { id }),
-    unlike: async (id) => fetchPosts(apiHost + '/posts/:postId/unlike', { id }),
-}
-}
+    const fetchPosts = useFetchPosts();
+    return {
+        login: (email, password) => fetchPosts(apiHost + '/users/login', { email, password }),
+        createPosts: (description, photo) => fetchPosts(apiHost + '/posts', { description, photo }),
+        like: async (id) => fetchPosts(apiHost + '/posts/:postId/like', { id }),
+        unlike: async (id) => fetchPosts(apiHost + '/posts/:postId/unlike', { id }),
+    };
+};
 
+export const listPosts = async (searchTerm) => {
+    const url = `http://localhost:3000/posts${searchTerm ? `?search=${searchTerm}` : ''}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Error al buscar publicaciones: ${response.statusText}`);
+    }
+    return await response.json();
+};
