6d8046a2f8bd0529f5dc77e2be141fb7e8c3e8dd
Marta <martagarciamgo@gmail.com>
2024-02-11 11:38:04 +0100

    más traducciones

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,33 +1,33 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
 //import { useUser } from "../../UserContext";
 
 const Register = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [password2, setPassword2] = useState('');
-  const [email, setEmail] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [userRegister, setUserRegister] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [userRegister, setUserRegister] = useState("");
 
   const handleForm = async (event) => {
     event.preventDefault();
-    const res = await fetch('http://localhost:3000/users/register', {
-      method: 'POST',
+    const res = await fetch("http://localhost:3000/users/register", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, email, password })
+      body: JSON.stringify({ username, email, password }),
     });
 
     const response = await res.json();
     if (res.ok) {
-      setUserRegister(true)
+      setUserRegister(true);
     } else {
       setErrorMessage("Ocurrió un error durante el registro.");
     }
   };
-
 
   if (userRegister) {
     return (
@@ -41,42 +41,44 @@ const Register = () => {
         name="username"
         placeholder="Usuario..."
         value={username}
-        onChange={e => setUsername(e.target.value)}
+        onChange={(e) => setUsername(e.target.value)}
       />
       <input
         name="email"
         placeholder="Email..."
         type="email"
         value={email}
-        onChange={e => setEmail(e.target.value)}
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <input 
+      <input
         type="password"
         placeholder="Contraseña..."
         value={password}
-        onChange={e => setPassword(e.target.value)}
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <input 
+      <input
         name="password"
         placeholder="Repetir contraseña..."
         type="password"
         value={password2}
-        onChange={e => setPassword2(e.target.value)}
+        onChange={(e) => setPassword2(e.target.value)}
       />
-      {password !== password2 &&
+      {password !== password2 && (
         <p className="error">Las contraseñas no coinciden.</p>
-      }
-      {errorMessage &&
-        <p className="error">{errorMessage}</p>
-      } 
+      )}
+      {errorMessage && <p className="error">{errorMessage}</p>}
 
-      <button>Registro</button> 
+      <button>
+        <FormattedMessage id="register.singUp" />
+      </button>
       <p>
-        ¿Ya tienes cuenta? <Link to="/login">Inicia sesión</Link>
+        <FormattedMessage id="register.account" />{" "}
+        <Link to="/login">
+          <FormattedMessage id="register.singIn" />
+        </Link>
       </p>
-    </form> 
+    </form>
   );
 };
 
 export default Register;
-
